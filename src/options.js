cd75d83155b001319513235e66daa44fe321ad7a
Henry Zhu <hi@henryzoo.com>
2016-03-18 09:04:38 -0400

    add option to clear data

--- src/options.js
+++ src/options.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const accessTokenInput = document.getElementById("token-input");
   const oauthLink = document.getElementById("use-oauth");
+  const clearCacheLink = document.getElementById("clear-cache");
 
   getSyncStorage({ "access_token": null })
   .then(({ access_token }) => {
@@ -16,4 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
   oauthLink.addEventListener("click", () => {
     getTokenFromOauth();
   })
+
+  clearCacheLink.addEventListener("click", () => {
+    let temp = accessTokenInput.value
+    chrome.storage.sync.clear((res) => {
+      setSyncStorage({ "access_token": temp });
+
+      document.querySelector("#feedback").textContent = "Storage Cleared";
+    });
+  })
 });
+
