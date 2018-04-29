07af7cd4490c16a72cd9155758d6e2dfc7a19efc
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-04-28 19:58:12 -0700

    allow user to enable on a domain. enables github enterprise

--- src/background.js
+++ src/background.js
@@ -1,4 +1,8 @@
-/* global chrome */
+/* global chrome DCE DCS */
+
+// GitHub Enterprise support
+DCE.addContextMenu();
+DCS.addToFutureTabs();
 
 // launch options page on first run
 chrome.runtime.onInstalled.addListener((details) => {
